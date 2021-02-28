7406975078002b8c4e2e69f30107c5efd7b247e4
Abanoub <abanoubtalaat30@gmail.com>
2021-02-28 14:55:28 +0200

    first commit

---
+++ components/News.js
@@ -0,0 +1,10 @@
+import NewsItem from '../components/NewsItem'
+import newsStyles from '../styles/News.module.css'
+const News = ({news}) => {
+    return (
+        <div className={newsStyles.grid}>
+        {news.news.map((newsItem)=><NewsItem newsItem={newsItem} />)}
+        </div>
+    )
+}
+export default News
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import News from '../components/News'
+
+export default function Home({news}) {
+  return (
+    <div >
+    {/* <Head>
+      <title>Webdev news</title>
+      <meta name='keywords' content='web development' />
+    </Head>
+     <h1>Welcome to home</h1> */}
+     <News news={news}/>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(` http://80.240.21.204:1337/news?skip=0&limit=100`)
+  const news = await res.json()
+  return {
+    props:{
+      news
+    }
+  }
+}
